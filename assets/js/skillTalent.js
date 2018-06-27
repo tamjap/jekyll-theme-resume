b746777e39fef8ef4a1ca2b9ba657d47957914d4
tamjap <tamjap@gmail.com>
2018-06-27 04:04:17 -0500

    Add layout for 'Experience'
    Add 'Experience' section to the resume, where users can list past
    jobs.  Layout is:
    
       COMPANY                                              City, ST
       title                                                   dates
          job description.....................................
          ....................................................
          ....................................................
    
    Modify skillTalent.js to require parameter for parent class and child
    class.  parentClass contains the class name for the first parent
    element in the DOM structure that contains both the button used to
    initiate hiding a div and the div to be hiden.  childClass contains the
    class name for the div to be hidden.  Technically, an ID or any valid
    JQuery selector could be passed into this routine.

--- assets/js/skillTalent.js
+++ assets/js/skillTalent.js
@@ -1,21 +1,15 @@
-function collapse(el) {
-    var primaryColor = $(el).css('background');
-    var secondaryColor = $(el).css('color');
+function collapse(el, parentClass, childClass) {
     var belowWidth = window.matchMedia("(max-width: 1300px)");
-    var parent = $(el).parent().find(".skillTalent");
-    var skillTalent = $(el).parent().next().find(".ulGroup");
+    var parent = $(el).parents(parentClass);
+    var skillTalent = $(parent).find(childClass);
     if (belowWidth.matches) {
         if ($(el).text()==='-') {
             $(el).text('+');
             $(skillTalent).css('display', 'none');
-            $(parent).css('background', secondaryColor);
-            $(parent).css('color', primaryColor);
         }
         else {
             $(el).text('-');
             $(skillTalent).css('display', 'inline-block');
-            $(parent).css('background', primaryColor);
-            $(parent).css('color', secondaryColor);
         }
     }
 };
